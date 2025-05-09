eb4c6b41db8e7ea5893393a0dbfbed77b9e9290a
synthematik <garfild_2003@mail.ru>
2025-05-09 12:45:21 +0300

    Update all html pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @articles = ::Article.all.sort_by(&:updated_at).last(5)
     @users = ::User.all
+    @articles_with_notification = ::Article.where(is_notification: true).sort_by(&:updated_at).last(5)
   end
 end
